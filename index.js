cac72b85d3c00dcaf9b3b0bc6c770835595a01b0
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-09 11:37:17 +0530

    update : made a route for /user/* requests

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express')
+const express = require('express');
+const { UserRouter } = require('./routes/users.routes');
 const app = express()
 const PORT = 9000;
 app.get('/', (req, res) => {
     res.json({status:true, message:"welcome to the homeage of my rest api"})
 })
 
-
+app.get('/user',UserRouter )
 app.listen(process.env.PORT || PORT, () => console.log("app is up and running"))
---
+++ routes/users.routes.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const { UserSignIn, UserSignUp } = require('./controllers/users')
+const UserRouter = express.Router()
+
+UserRouter.route('/signin', UserSignIn)
+UserRouter.route('/signup', UserSignUp)
+module.exports = {UserRouter}
