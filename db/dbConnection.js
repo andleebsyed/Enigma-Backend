68d3b8726630553fe8172b493becb8918d90508c
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-09 15:33:50 +0530

    added useCreateIndex option

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 function DbConnection(){
-  mongoose.connect(process.env.URI, {useNewUrlParser : true , useUnifiedTopology : true})
+  mongoose.connect(process.env.URI, {useNewUrlParser : true , useUnifiedTopology : true, useCreateIndex: true})
 .then(res => console.log("connected to database"))
 .catch(error => console.log("error occured " , error))
 }
