ceea507546616ffb759c2df45ed3f6b675521372
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-09 13:00:07 +0530

    update : configured database with the project

---
+++ dbConnection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+function DbConnection(){
+  mongoose.connect(process.env.URI, {useNewUrlParser : true , useUnifiedTopology : true})
+.then(res => console.log("connected to database"))
+.catch(error => console.log("error occured " , error))
+}
+module.exports = {DbConnection}
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const { UserRouter } = require('./routes/users.routes');
+require('dotenv').config()
+const { DbConnection } = require('./dbConnection');
+const { UserRouter } = require('./routes/user.routes');
 const app = express()
 const PORT = 9000;
+// connecting to mongodb
+DbConnection()
 app.get('/', (req, res) => {
     res.json({status:true, message:"welcome to the homeage of my rest api"})
 })
