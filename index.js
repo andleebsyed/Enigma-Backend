25b5159a049eeba6f59eba1ba650286cc4025cf2
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-09 10:15:06 +0530

    update : added env PORT

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-
+const PORT = 9000;
 app.get('/', (req, res) => {
     res.json({status:true, message:"welcome to the homeage of my rest api"})
 })
 
 
-app.listen(9000, () => console.log("app is up and running"))
+app.listen(process.env.PORT || PORT, () => console.log("app is up and running"))
