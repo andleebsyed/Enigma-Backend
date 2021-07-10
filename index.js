2b48510c55435275ce6502422b15542e641d13c7
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-10 19:08:45 +0530

    refactor : function which added data to db commented out so that data won't get added again on subsequent refreshes

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const cors = require('cors')
 require('dotenv').config()
 const { DbConnection } = require('./db/dbConnection');
 const { UserRouter } = require('./routes/user.routes');
+const {InsertDataToDatabase} = require('./controllers/quizCategoriesAdd')
 const app = express()
 const PORT = 9000;
 // connecting to mongodb
 DbConnection()
+//Insertion of initial data
+// InsertDataToDatabase()
 app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => {
