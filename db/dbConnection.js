8dc84447e3f96b81d902579348176a5001148eaa
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-24 23:44:46 +0530

    update: changed async operation from .then syntax to async/await

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
-function DbConnection(){
-  mongoose.connect(process.env.URI, {useNewUrlParser : true , useUnifiedTopology : true, useCreateIndex: true})
-.then(res => console.log("connected to database"))
-.catch(error => console.log("error occured " , error))
+async function DbConnection() {
+  try {
+    await mongoose.connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    console.log("connected to database")
+  }
+  catch (error) {
+    console.log("error occured while connecting to database: ", error?.message, error)
+  }
 }
 module.exports = {DbConnection}
